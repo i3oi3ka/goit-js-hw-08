ce221a4b4556696a73fbbe4ae6f0ea921a720272
i3oi3ka <i3oi3ka@ukr.net>
2025-02-17 23:42:12 +0200

    complete homework 8;

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputName = document.querySelector('#name-input');
-
-inputName.addEventListener('input', outputHandle);
-function outputHandle(event) {
-  const outputName = document.querySelector('#name-output');
-  const name = event.currentTarget.value.trim();
-  outputName.textContent = name || 'Anonymous';
-}
